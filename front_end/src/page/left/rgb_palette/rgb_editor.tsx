3458b1f99a7400d9d90d72f25ec37c1ed426adba
Turingraph <nemocheche@gmail.com>
2026-03-15 12:32:57 +0700

    select all button

--- front_end/src/page/left/rgb_palette/rgb_editor.tsx
+++ front_end/src/page/left/rgb_palette/rgb_editor.tsx
@@ -12,17 +12,30 @@ export default function RGB_EDITOR({
 {
 	const {ss: SS_RGBArr, setss: setSS_RGBArr} = useContext(CX_SS_PALETTE).rgb_arr
 	const [SS_UpdateRGBArr, setSS_UpdateRGBArr] = useReducer(act_arr, [...SS_RGBArr])
+	const f_select_all = function(select:boolean){
+		const update_rgb_arr = [...SS_UpdateRGBArr]
+		let i = 0
+		while (i < SS_UpdateRGBArr.length)
+		{
+			update_rgb_arr[i].select = select
+			i += 1
+		}
+		setSS_UpdateRGBArr({type:"SET", input:update_rgb_arr})
+	}
 	return <RGB_TABLE
 		editor_or_picker={undefined}
 		rgb_arr={{ss:SS_UpdateRGBArr, setss:setSS_UpdateRGBArr}}
 		f_clicks={[
-			{title:"Select All", func:(()=>{console.log("Darken")}) as a.t_func},
-			{title:"Unselect All", func:(()=>{console.log("Darken")}) as a.t_func},
+			{title:"Select All", func:(()=>{f_select_all(true)}) as a.t_func},
+			{title:"Unselect All", func:(()=>{f_select_all(false)}) as a.t_func},
 			{title:"Modify Color", func:(()=>{console.log("Darken")}) as a.t_func},
 			{title:"Save", func:(()=>{
+				f_select_all(false)
 				setSS_RGBArr({type:"SET", input:SS_UpdateRGBArr})
 				change_ui_mode()}) as a.t_func},
-			{title:"Cancel", func:(()=>{change_ui_mode()}) as a.t_func},
+			{title:"Cancel", func:(()=>{
+				f_select_all(false)
+				change_ui_mode()}) as a.t_func},
 		]}
 	/>
 }
